17d158173f3605eb846eb698526d8b32bcbf13dc
wn1980 <>
2019-07-31 06:47:28 +0700

    update

--- robot/run-gpu.sh
+++ robot/run-gpu.sh
@@ -4,18 +4,16 @@ p1080=1920x1080
 p720=1280x720
 p169=1600x900
 
-NAME=ros-robot
+NAME=ros-robot-daemon
 
 docker rm -f $NAME
 
-docker run -d \
+nvidia-docker run -d \
 	-p 5901:5901 \
 	-p 6901:6901 \
 	--privileged \
-	--runtime=nvidia \
-	--ipc=host \
 	--volume="$PWD:$HOME" \
-	-e NVIDIA_VISIBLE_DEVICES=0 \
 	-e VNC_RESOLUTION=$p1080 \
 	--name $NAME \
 	wn1980/ros-robot:gpu-rc
+#	--restart unless-stopped \
