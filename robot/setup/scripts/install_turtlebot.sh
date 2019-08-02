c052f2d67bb4f8a0c838f791750db33906a41f6b
wn1980 <50045716+wn1980@users.noreply.github.com>
2019-08-02 11:00:50 +0700

    Update install_turtlebot.sh

--- robot/setup/scripts/install_turtlebot.sh
+++ robot/setup/scripts/install_turtlebot.sh
@@ -21,14 +21,14 @@ git clone https://github.com/turtlebot/turtlebot_interactions.git
 git clone https://github.com/ros-drivers/freenect_stack
 git clone https://github.com/ros-drivers/linux_peripheral_interfaces
 
-git clone https://github.com/toeklk/orocos-bayesian-filtering.git
-cd orocos-bayesian-filtering/orocos_bfl/
-./configure
-make
-sudo make install
-cd ../
-make
-cd ../
+#git clone https://github.com/toeklk/orocos-bayesian-filtering.git
+#cd orocos-bayesian-filtering/orocos_bfl/
+#./configure
+#make
+#sudo make install
+#cd ../
+#make
+#cd ../
 
 #git clone https://github.com/udacity/robot_pose_ekf
 #git clone https://github.com/ros-perception/depthimage_to_laserscan.git
@@ -47,10 +47,11 @@ git clone https://github.com/yujinrobot/yujin_ocs.git
 mv yujin_ocs/yocs_cmd_vel_mux yujin_ocs/yocs_controllers .
 rm -rf yujin_ocs
 
+sudo apt-get install ros-melodic-bfl -y
 sudo apt-get install ros-melodic-kobuki-* -y
 sudo apt-get install ros-melodic-ecl-streams -y
 
-echo '#\n TurtleBot settings' >> ~/.bashrc
+echo '\n# TurtleBot settings' >> ~/.bashrc
 
 echo 'export TURTLEBOT_MAP_FILE=~/my_map.yaml' >> ~/.bashrc
 
