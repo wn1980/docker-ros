71144256937f4ffb349d1157688c171158922c9f
wn1980 <50045716+wn1980@users.noreply.github.com>
2019-08-05 04:13:48 +0700

    Update dl_pkg.sh

--- robot/setup/scripts/dl_pkg.sh
+++ robot/setup/scripts/dl_pkg.sh
@@ -3,13 +3,13 @@
 if [ $(uname -m) == 'x86_64' ] 
 then
 	echo x86_64
-	$HOME/.scripts/gdown.pl 'https://drive.google.com/file/d/1Ch3-hh3ClKVa-kU9NoqmHKMpmX0PbvFZ/view?usp=drivesdk' '$HOME/tmp.tar.gz'
+	$HOME/.scripts/gdown.pl https://drive.google.com/file/d/1Ch3-hh3ClKVa-kU9NoqmHKMpmX0PbvFZ $HOME/tmp.tar.gz
 	tar -xzvf $HOME/tmp.tar.gz -C $HOME
 	rm -f $HOME/tmp.tar.gz
 elif [ $(uname -m) == 'aarch64' ] 
 then 
 	echo aarch64
-	$HOME/.scripts/gdown.pl 'https://drive.google.com/file/d/1PASQWl-v8LCjn2kjJTNSoN_ksez7mdBo/view?usp=drivesdk' '$HOME/tmp.tar.gz' 
+	$HOME/.scripts/gdown.pl https://drive.google.com/file/d/1PASQWl-v8LCjn2kjJTNSoN_ksez7mdBo $HOME/tmp.tar.gz 
 	tar -xzvf $HOME/tmp.tar.gz -C $HOME
 	rm -f $HOME/tmp.tar.gz
 else
