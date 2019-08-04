1dca9b0b490235ec68fa4e17c7cf84f0e687c7c8
wn1980 <50045716+wn1980@users.noreply.github.com>
2019-08-04 19:20:55 +0700

    Update dl_pkg.sh

--- robot/setup/scripts/dl_pkg.sh
+++ robot/setup/scripts/dl_pkg.sh
@@ -9,7 +9,7 @@ then
 elif [ $(uname -m) == 'aarch64' ] 
 then 
 	echo aarch64
-	./gdown.pl 'https://drive.google.com/open?id=1Ch3-hh3ClKVa-kU9NoqmHKMpmX0PbvFZ' '$HOME/tmp.tar.gz' 
+	./gdown.pl 'https://drive.google.com/file/d/1PASQWl-v8LCjn2kjJTNSoN_ksez7mdBo/view?usp=drivesdk' '$HOME/tmp.tar.gz' 
 	tar -xzvf $HOME/tmp.tar.gz -C $HOME
 	rm -f $HOME/tmp.tar.gz
 else
